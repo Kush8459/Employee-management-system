e23dfbe0b09ebd174c84f3dfc4726aea64a49230
Kush8459 <agrawalkush04@gmail.com>
2024-12-22 21:28:55 +0530

    stored data in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import { useEffect } from "react";
 import Login from "./components/auth/Login";
 import EmployeeDashboard from "./components/dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/dashboard/AdminDashboard";
+import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
 
 const App = () => {
+  useEffect(() => {
+    setLocalStorage();
+    getLocalStorage();
+  });
+
   return (
     <>
-      <Login />;
-      {/* <EmployeeDashboard /> */}
+      <Login />;{/* <EmployeeDashboard /> */}
       {/* <AdminDashboard /> */}
     </>
   );
